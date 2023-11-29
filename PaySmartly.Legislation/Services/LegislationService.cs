6e515ee91d7691d5793ef9a724fbf1b25cbb9a02
stefan <stefan.bozov@outlook.com>
2023-11-29 06:49:59 +0200

    implement LegislationService

--- PaySmartly.Legislation/Services/LegislationService.cs
+++ PaySmartly.Legislation/Services/LegislationService.cs
@@ -2,15 +2,31 @@ using Grpc.Core;
 
 namespace PaySmartly.Legislation.Services;
 
-public class LegislationService(ILogger<LegislationService> logger) : Legislation.LegislationBase
+public class LegislationService : Legislation.LegislationBase
 {
-    private readonly ILogger<LegislationService> _logger = logger;
+    private readonly ILogger<LegislationService> _logger;
+
+    private readonly TaxableIncomeTable taxableIncomeTable;
+
+    public LegislationService(ILogger<LegislationService> logger)
+    {
+        _logger = logger;
+
+        TaxableRange range1 = new() { Start = 0, End = 14_000, Tax = 0.105 };
+        TaxableRange range2 = new() { Start = 14_001, End = 48_000, Tax = 0.175 };
+        TaxableRange range3 = new() { Start = 48_001, End = 70_000, Tax = 0.3 };
+        TaxableRange range4 = new() { Start = 70_001, End = 180_000, Tax = 0.33 };
+        TaxableRange range5 = new() { Start = 180_001, End = double.MaxValue, Tax = 0.39 };
+
+        taxableIncomeTable = new TaxableIncomeTable();
+        taxableIncomeTable.Ranges.AddRange([range1, range2, range3, range4, range5]);
+    }
 
     public override Task<Response> GetTable(Request request, ServerCallContext context)
     {
         return Task.FromResult(new Response
         {
-            Message = "Hello " + request.Name
+            Table = taxableIncomeTable
         });
     }
 }
